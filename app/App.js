b268ca8f6af94cbdf4f737987da4542137ec8538
Álvaro Márquez <marquezalvaro13@gmail.com>
2018-02-13 22:05:43 +0100

    Add styles

--- app/App.js
+++ app/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Dropdown from './components/Dropdown'
-
+import './css/app.css';
 
 const states = require('../db.json');
 
